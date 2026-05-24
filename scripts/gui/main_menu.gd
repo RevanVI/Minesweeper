06e49a06c93d65bb8cf5dd02f9848c3688767f61
RevanVI <47805713+RevanVI@users.noreply.github.com>
2026-05-24 12:55:40 +0300

    Cleanup magic numbers

--- scripts/gui/main_menu.gd
+++ scripts/gui/main_menu.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 
 
 func _on_endless_mode_btn_pressed() -> void:
-	var difficulty_level: int = randi_range(0, 2)
+	var difficulty_level: int = randi_range(EndlessGameModeManager.Diffuculty.EASY, EndlessGameModeManager.Diffuculty.HARD)
 	var data: Dictionary = {"difficulty_level": difficulty_level}
 	SceneSwitcherGlobal.switch_scene("res://scenes/modes/endless_mode.tscn", "endless_mode_settings", data)
 
@@ -20,7 +20,7 @@ func _on_exit_btn_pressed() -> void:
 
 
 func _on_classic_mode_btn_pressed() -> void:
-	var difficulty_level: int = randi_range(0, 2)
+	var difficulty_level: int = randi_range(ClassicGameModeManager.Diffuculty.EASY, ClassicGameModeManager.Diffuculty.HARD)
 	var data: Dictionary = {"difficulty_level": difficulty_level}
 	SceneSwitcherGlobal.switch_scene("res://scenes/modes/classic_mode.tscn", "classic_mode_settings", data)
 
